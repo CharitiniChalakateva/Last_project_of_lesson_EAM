217efd49c3cff10f3a90a170e0d9a85f2e0698d8
__charitini_chalakateva__ <100290749+CharitiniChalakateva@users.noreply.github.com>
2023-12-21 18:40:20 +0200

    code

--- code/DASHBOARD.vue
+++ code/DASHBOARD.vue
@@ -1,21 +1,67 @@
 <template>
   <div>
-    <h1>Available Courses</h1>
-    <ul>
-      <li v-for="course in courses" :key="course.id">
-        {{ course.title }}
-      </li>
-    </ul>
-    <h1>Available Grades</h1>
-    <ul>
-      <li v-for="exam in exams" :key="exam.id">
-        {{ exam.title }}
-      </li>
-    </ul>
-    <h1>User Profile</h1>
-    <div>
+    <h1>Home Page</h1>
+    <!-- Εδώ μπορείτε να προσθέσετε λογική για τον έλεγχο του ρόλου του χρήστη -->
+    <div v-if="user.role === 'student'">
+      <h2>Students</h2>
+      <p>Here you can see the available courses:</p>
+      <ul>
+        <li v-for="course in courses" :key="course.id">
+          {{ course.title }}
+        </li>
+      </ul>
+      <p>Here you can see the available courses:</p>
+      <ul>
+        <li v-for="exam in exams" :key="exam.id">
+          {{ exam.title }}
+        </li>
+      </ul>
+      <h3>Student Profile</h3>
       <p>Username: {{ user.username }}</p>
       <p>Email: {{ user.email }}</p>
+    </div>
+
+    <div v-else-if="user.role === 'professor'">
+      <h2>Professor</h2>
+      <p>Here you can see the courses you teach:</p>
+      <ul>
+        <li v-for="course in courses" :key="course.id">
+          {{ course.title }}
+        </li>
+      </ul>
+      <p>Here you can see the exam results:</p>
+      <ul>
+        <li v-for="exam in exams" :key="exam.id">
+          {{ exam.title }}
+        </li>
+      </ul>
+      <h3>Professor Profile</h3>
+      <p>Username: {{ user.username }}</p>
+      <p>Email: {{ user.email }}</p>
+    </div>
+
+    <div v-else-if="user.role === 'secretariat'">
+      <h2>Secretariat</h2>
+      <p>Here you can see the courses you handle:</p>
+      <ul>
+        <li v-for="course in courses" :key="course.id">
+          {{ course.title }}
+        </li>
+      </ul>
+      <p>Here you can see the exam results:</p>
+      <ul>
+        <li v-for="exam in exams" :key="exam.id">
+          {{ exam.title }}
+        </li>
+      </ul>
+      <h3>Secretariat Profile</h3>
+      <p>Username: {{ user.username }}</p>
+      <p>Email: {{ user.email }}</p>
+    </div>
+
+    <div v-else>
+      <h2>Visitors</h2>
+      <p>Welcome! Please log in to gain access.</p>
     </div>
   </div>
 </template>
