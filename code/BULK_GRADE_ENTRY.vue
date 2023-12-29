f1bff70e8cc44ff97ef2483452b6ad69c7a60ede
__charitini_chalakateva__ <100290749+CharitiniChalakateva@users.noreply.github.com>
2023-12-29 15:57:15 +0200

    code

--- code/BULK_GRADE_ENTRY.vue
+++ code/BULK_GRADE_ENTRY.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <h1>Bulk Grade Entry</h1>
+    <!-- Prevents the default form submission and invokes the handleFileUpload method -->
     <form @submit.prevent="handleFileUpload">
       <div>
         <label for="gradeFile">Upload File:</label>
-        <!-- Accept only Excel files -->
+        <!-- Accept only Excel files (.xlsx) -->
         <input type="file" id="gradeFile" ref="gradeFile" accept=".xlsx" /> 
         
       </div>
@@ -18,6 +19,7 @@
 <script>
 export default {
   methods: {
+    // Handles the file upload when the form is submitted
     handleFileUpload() {
       const fileInput = this.$refs.gradeFile;
 
